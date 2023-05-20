cee798b5fdd6037b4f6f60365246c7e84988d11e
Yann Richard <yann.richard9@gmail.com>
2023-05-20 22:22:51 +0200

    added a new route POST allowing to create new users

--- userHandlers.js
+++ userHandlers.js
@@ -10,7 +10,7 @@ const getUsers = (req, res) => {
 .catch((err) => {
 
   console.error(err);
-
+cd 
   res.status(500).send("Error retrieving data from database");
 
 });
@@ -35,4 +35,54 @@ const getUserById = (req,res) => {
 })
 }
 
-module.exports = {getUsers, getUserById}
+const postUsers = (req,res) => {
+  const { firstname, lastname, email, city, language } = req.body
+
+
+database
+.query(
+  "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?,?,?,?,?)", 
+  [firstname, lastname, email, city, language])
+.then(([result]) => {
+res.location(`/api/users/${result.insertId}`).sendStatus(201);
+})
+.catch((err) => {
+  console.error(err); 
+  res.status(500).send("Error saving the user")
+})
+}
+
+const deleteUsers = (req, res) => {
+
+  const id = parseInt(req.params.id);
+
+
+  database
+
+    .query("delete from users where id = ?", [id])
+
+    .then(([result]) => {
+
+      if (result.affectedRows === 0) {
+
+        res.status(404).send("Not Found");
+
+      } else {
+
+        res.sendStatus(204);
+
+      }
+
+    })
+
+    .catch((err) => {
+
+      console.error(err);
+
+      res.status(500).send("Error deleting the user");
+
+    });
+
+};
+
+module.exports = {getUsers, getUserById, postUsers, deleteUsers}
